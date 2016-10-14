3c87e804d1528579af1339f40519603c762e5dc6
Pedro Araujo <ppteixeira@outlook.com>
2016-10-14 12:47:04 +1100

    fixed few thing, got the hover working partially

---
+++ README.md
@@ -0,0 +1,40 @@
+#README
+
+##About the game project
+
+  My game chosen for this project was Tic Tac Toe.
+  The project was made using Javascript , Jquery , HTML and CSS.
+
+
+##The game
+
+  This is a simple Tic Tac Toe game, which takes the user's input in one of the 9 spaces.
+  The players take turns choosing the spaces to place your selection and when there is a line of three consecutive selections of the same type horizontally, vertically or diagonally the game shows who is the winner and keeps the players scores.
+
+###Storing inputs
+
+  The approach taken was creating an array of 9 values.
+  Every time a player select one of the boxes, the value for that position in the array is changed.
+
+  The other inputs asked at the beginning of the game is both players's names and a selection (for player 1) of what option he wants between crosses and noughts.
+
+###Find the winner
+
+  To find a winner is necessary to compare two arrays:
+    1 - An array that store the index of all the selections made by the user.
+    2 - An array that has one of the valid combination to determine the winner.
+
+  So when compared, if inside the array with indexes of the user's input are contained the same indexes of the valid combination, it returns a winner.
+  The first array is compared with the eight possible combinations.
+
+###The Layout
+
+  I tried to make it look like a casual game between friends in a place that
+  could be an Office, School or even at home. Where there is a table with few belongings.
+
+###Future Additions
+
+  On the future, I would like to add the option of themes for the users to selecet, that would change the way that the whole page looks.
+  Changing Background, board and figures.
+
+  I also would like to add possibilities to increase the number of squares to add complexity to the game.
--- scripts/main3.js
+++ scripts/main3.js
@@ -1,48 +1,139 @@
 console.log('it is working');
-var cards = ['a', 'b', 'a', 'b'];
 var board = ['', '', '', '', '', '', '', '', '' ];
 var tokenPlayer = 1;
+var tokenP1Char = null;
 var totalMoves = 0;
-var player1 = {p1name: 'Player 1', score: 0}
-var player2 = {p2name: 'Player 2', score: 0}
+var player1 = {nick: '' , score: 0};
+var player2 = {nick: '' , score: 0};
 var roundWinner = null;
 var $p1Div = $('#player1');
-var $p1Name = $('<p>').attr('id','p1Name').text(player1.p1name);
-var $p1Score = $('<p>').attr('id','p1Score').text('Score: '+ player1.score);
-$p1Div.append($p1Name,$p1Score);
 var $p2Div = $('#player2');
-var $p2Name = $('<p>').attr('id','p2Name').text(player2.p2name);
-var $p2Score = $('<p>').attr('id','p2Score').text('Score: '+ player2.score);
-$p2Div.append($p2Name,$p2Score);
+var classP1 = null;
+var classP2 = null;
+
+
+//___________________Button to start playing and get values from Form __________
+
+$('#btnStart').click(function () {
+  player1.nick = $('#inputP1Name').val();
+  player2.nick = $('#inputP2Name').val();
+  tokenP1Char = $(':checked').attr('value');
+  tokenP1Char = Number(tokenP1Char)
+  $('.form').css('display',"none");
+  $('.game').css('display','block');
+
+  // Display Player 1 Name and Score
+  var $p1Name = $('<p>').attr('id','p1Name').text(player1.nick );
+  var $p1Score = $('<p>').attr('id','p1Score').text('Score: '+ player1.score);
+  $p1Div.append($p1Name,$p1Score);
+
+  // Display Player 2 Name and Score
+
+  var $p2Name = $('<p>').attr('id','p2Name').text(player2.nick);
+  var $p2Score = $('<p>').attr('id','p2Score').text('Score: '+ player2.score);
+  $p2Div.append($p2Name,$p2Score);
+
+  if (tokenP1Char == 1) { //Tells what Character player 1 selected
+    hoverP1 = 'hoverCross'     // vars to be used when hovering squares
+    hoverP2 = 'hoverNought'
+  }else {
+    hoverP1 = 'hoverNought'
+    hoverP2 = 'hoverCross'
+  }
+
+})
+
+
+
+
+
+//__________________________________HOVER____________________________________
+
+function hover (){
+  $('.board').mouseover( function (event) {
+    var $empty = $(event.target).attr('empty');
+
+    if ($empty == 'true'){
+      if (tokenPlayer == 1) {
+        $( event.target ).addClass(hoverP1);
+      }else {
+        $( event.target ).addClass(hoverP2);
+      }
+    }
+  });
+  $('.board').mouseout( function (event) {
+
+      if (tokenPlayer == 1) {
+        $( event.target ).removeClass(hoverP1);
+      }else {
+        $( event.target ).removeClass(hoverP2);
+      }
+
+  });
+
+}
+hover();
 
 //______________________________Event Listener________________________
 function addEventListener() {
   $('.board').on('click', function(event){
     // update move
     var $index = $(event.target).attr('data-index'); // store data-index of event.target
-    var $empty = $(event.target).attr('empty');
+    var $empty = $(event.target).attr('empty');     // store empty value of event.target
     var result = null;
-    if ($empty == 'true') {
-      if (tokenPlayer == 1) {
-        result = clickBox('X',$index);
+
+    if (tokenP1Char == 1) { //Tells what Character player 1 selected
+      var printP1 = 'X'
+      var printP2 = 'O'
+    }else {
+      var printP1 = 'O'
+      var printP2 = 'X'
+    }
+
+
+    if ($empty == 'true') {      // If ther's nothing inside the square
+      if (tokenPlayer == 1) {   // Check who's playing (Player 1)
+        result = clickBox(printP1,$index); // Call function to select the square
+                                           // and wait for result
+
         if (result == true) { // if there is a winner
-         player1.score = player1.score + 1;
-         $('#p1Score').text('Score: ' + player1.score)
-          roundWinner = 'Crosses';
+          player1.score = player1.score + 1;
+          $('#p1Name').text(player1.nick);
+          $('#p1Score').text('Score: ' + player1.score);
           // Stop event listener (game over)
           $('.board').off();
+          $('.displayResult').text(player1.nick + ' Win!').css("color", "#dd3044");
+          var $eraser = $('<button>').attr({id:"resetBtn",type:"button"}).text('Play Again!');
+          $('.eraser').append($eraser);
+        }else if (result == false && totalMoves == 9) {
+          $('.board').off();
+          $('.displayResult').text('Tie game!').css("color", "grey");
+          var $eraser = $('<button>').attr({id:"resetBtn",type:"button"}).text('Play Again!');
+          $('.eraser').append($eraser);
+          $('.square').removeClass('hoverNought','hoverCross');
+
         }
+
       }else {
-        result = clickBox('O',$index);
+        result = clickBox(printP2,$index);
         if (result == true) { // if there is a winner
           player2.score = player2.score + 1;
+          $('#p2Name').text(player2.nick)
            $('#p2Score').text('Score: ' + player2.score)
-          roundWinner = 'Crosses';
           // Stop event listener (game over)
           $('.board').off();
+          $('.displayResult').text(player2.nick + ' Win!').css("color", "navy");
+          var $eraser = $('<button>').attr({id:"resetBtn",type:"button"}).text('Play Again!');
+          $('.eraser').append($eraser);
+        }else if (result == false && totalMoves == 9) {
+          $('.board').off();
+          $('.displayResult').text('Tie game!').css("color", "grey");
+          var $eraser = $('<button>').attr({id:"resetBtn",type:"button"}).text('Play Again!');
+          $('.eraser').append($eraser);
+          $('.square').removeClass('hoverNought','hoverCross');
         }
       }
-
+      hover();
     }
   });
 };
@@ -53,9 +144,7 @@ addEventListener();
 function clickBox(key,$index){
   board[$index] = key;
   var $target = $(event.target)
-  var $pTarget = $('<p>').text(key)
-
-  $target.append($pTarget);
+  $target.text(key);
   $target.attr('empty','false');
   if (tokenPlayer == 1) {
     $target.addClass('cross');
@@ -84,10 +173,10 @@ function checkWinner(key){
 
 }
 
-//_______________________ Get the indexes of the moves made_______________
+//_______________ Get the indexes of the moves made by the player_______________
 //  --> Called by CheckWinner function
 function getAllIndexes(arr, val) {
-    var indexes = [], i;            // by the player
+    var indexes = [], i;
     for(i = 0; i < arr.length; i++){
       if (arr[i] === val)
       indexes.push(i);
@@ -95,7 +184,7 @@ function getAllIndexes(arr, val) {
     return indexes;               // return the indexes back to checkWinner
 }
 
-//____Creates the possibilites to be compared with the moves made by the players__
+//___Creates the possibilites to be compared with the moves made by the players__
 // --> Called by CheckWinner function
 function mathWinner(arr) {
   var hr1 = [0,1,2];
@@ -121,8 +210,10 @@ function mathWinner(arr) {
 
 //____Compare if the content of an arran is contained in another array_________
 // --> Called by mathWinner
-function containsAll(possibilities, moves){ //   ( possibilities, player's moves)
+
+function containsAll(possibilities, moves){ // ( possibilities, player's moves)
   for(var i = 0 , len = possibilities.length; i < len; i++){
+
     // If it does not find the possibilities into the moves, return false.
     if($.inArray(possibilities[i], moves) == -1) return false;
   }
@@ -131,21 +222,40 @@ function containsAll(possibilities, moves){ //   ( possibilities, player's moves
 
 //______________________Draw board for the first time__________________________
 for (var i = 0; i < board.length; i++) {
-  $('.board').append($('<div>').attr({'data-index':i ,'empty':'true','class':'square'}).text(board[i]));
+  $('.board').append($('<div>').attr({'data-index':i ,'empty':'true','class':'square','id': 'sqr-'+i}).text(board[i]));
 }
-$('#resetBtn').click( function(){
+
+//___________________________________Play Again Button________________________
+// Clear all VALUES (except scores)
+$('.eraser').click('button' ,function(){
+  totalMoves = 0;                   //clear totalMoves
   var $allSquares = $('.square')
-  $.each($allSquares,function(){
+  $.each($allSquares,function(){   // clear the squares removing classes and data
     $(this).removeClass('nought');
     $(this).removeClass('cross');
     $(this).attr('empty','true');
+    $('.displayResult').text('');  // clear text from displayResult
     //redraw board
-    var board = ['', '', '', '', '', '', '', '', '' ];
-    $('.board').empty();
-    addEventListener();
-    for (var i = 0; i < board.length; i++) {
-      $('.board').append($('<div>').attr({'data-index':i ,'empty':'true','class':'square'}).text(board[i]));
+    board = ['', '', '', '', '', '', '', '', '' ]; // clear board
+    $('.board').empty();                          // empty div Board
+    for (var i = 0; i < board.length; i++) {      // generate squares divs
+      $('.board').append($('<div>').attr({'data-index':i ,'empty':'true','class':'square','id': 'sqr-'+i}).text(board[i]));
     }
-
+    addEventListener();       // Add Event Listener to them
+    $('#resetBtn').remove();  // hide (remove) 'Play Again' buttom
   });
 });
+
+//
+// sessionStorage.setItem('boardGame', board);
+// sessionStorage.setItem('player1Score', player1.score);
+// sessionStorage.setItem('player1Name', player1.name);
+// sessionStorage.setItem('player2Score', player2.score);
+// sessionStorage.setItem('player2Name', player2.name);
+//   if (sessionStorage.getItem('autosave')){
+//     board = sessionStorage.getItem('boardGame');
+//     player1.score = sessionStorage.getItem('player1Score');
+//     player2.score = sessionStorage.getItem('player2Score');
+//     player1.name = sessionStorage.getItem('player1Name');
+//     player2.name = sessionStorage.getItem('player2Name');
+//   }
